d620827431236004eeea74f9396f4cccd3546cb2
ZSchweyk <zeynschweyk@dpengineering.org>
2022-05-01 16:17:57 -0700

    commented out prints

--- memoization.py
+++ memoization.py
@@ -1,10 +1,11 @@
 def fib(n, computed={1: 1, 2: 1}):
-     if n not in computed:
-         computed[n] = fib(n-1, computed) + fib(n-2, computed)
+    if n not in computed:
+        # print(f"{n} DNE")
+        computed[n] = fib(n-1, computed) + fib(n-2, computed)
+    # print(f"{n} E")
+    return computed[n]
 
-     return computed[n]
-
-
-for i in range(1, 1001):
-    print(i, fib(i), sep=": ")
+print(fib(1000))
+# for i in range(1, 1001):
+#     print(i, fib(i), sep=": ")
 
